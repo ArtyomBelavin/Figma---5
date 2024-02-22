25250b667e7e4e0708e40889864b225217351671
ArtyomBelavin <153841043+ArtyomBelavin@users.noreply.github.com>
2024-02-22 16:19:12 +0500

    Delete loop.local-Белавин Артём directory

--- loop.local-Белавин Артём/js/javascript_1.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById("navbar-toggler");
-        const menus = document.querySelectorAll(".menu");
-        btn.addEventListener('click',() => {
-            for (let el of menus) {
-                el.classList.toggle("show");
-            }
-        })
